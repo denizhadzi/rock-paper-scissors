4b11f085d38aa281891e345220286318097cf31b
Deniz <denizhadzi@gmail.com>
2023-03-28 23:46:23 +0200

    Add RPS.js and index.html, we dont need 5 round to announce the winner

--- RPS.js
+++ RPS.js
@@ -11,15 +11,21 @@ function game() {
         for (let i = 0; i < 5; i++) {
             SCORE[playRound()]++;
             console.log(SCORE);
-        }
-        if (SCORE.PW > SCORE.CW || SCORE.PW >= 3) {
-            alert(`Congratulations, you won the game!!! \n PLAYER:${SCORE.PW}\n COMPUTER:${SCORE.CW}\n DRAW:${SCORE.DRAW} `)
-        }
-        else if (SCORE.CW > SCORE.PW || SCORE.CW >= 3) {
-            alert(`Sorry, you lost!!! \n COMPUTER:${SCORE.CW}\n PLAYER:${SCORE.PW}\n DRAW:${SCORE.DRAW}\n `)
-        }
-        else {
-            alert(`Ehh there is no winner!!!\n PLAYER:${SCORE.PW}\n COMPUTER:${SCORE.CW}\n DRAW:${SCORE.DRAW} `)
+            if (SCORE.PW >= 3 || (SCORE.PW + SCORE.DRAW > 3 && (SCORE.DRAW == 2 || SCORE.DRAW == 4)) && SCORE.DRAW - SCORE.PW != 4 ) {
+                alert(`Congratulations, you won the game!!! \n PLAYER:${SCORE.PW}\n COMPUTER:${SCORE.CW}\n DRAW:${SCORE.DRAW} `)
+                console.log(SCORE);
+                return;
+            }
+            if (SCORE.CW >= 3 || (SCORE.CW + SCORE.DRAW > 3 && (SCORE.DRAW == 2 || SCORE.DRAW == 4)) && SCORE.DRAW - SCORE.CW != 4 ) {
+                alert(`Sorry, you lost!!! \n COMPUTER:${SCORE.CW}\n PLAYER:${SCORE.PW}\n DRAW:${SCORE.DRAW}\n `)
+                console.log(SCORE);
+                return;
+            }
+            else if ((SCORE.PW + SCORE.CW  + SCORE.DRAW == 5) ) {
+                alert(`Ehh there is no winner!!!\n PLAYER:${SCORE.PW}\n COMPUTER:${SCORE.CW}\n DRAW:${SCORE.DRAW} `)
+                console.log(SCORE);
+            }
+            
         }
     }
 
@@ -58,10 +64,7 @@ function getPlayerChoice() {
     }
     else {
        return getPlayerChoice();
-    }
-
-
-    
+    } 
 }
 
 function getComputerChoice() {
